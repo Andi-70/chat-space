93e2b89a614d9d73c404d9ae90e8f9b46f318c5a
Yosuke Sawa <sawa.tno@gmail.com>
2019-08-11 19:36:34 +0900

    setting_SingIn-Out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+
+  get 'users/update'
+
+  devise_for :users
   root 'messages#index'
   resources :messages
+  resources :users, only: [:edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
