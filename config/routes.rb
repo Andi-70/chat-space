9069899406e7e6b0eb38925efd510d178d4a9659
Yosuke Sawa <sawa.tno@gmail.com>
2019-08-12 13:53:05 +0900

    define-models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :messages
-  resources :users, only: [:edit, :update]
-
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
